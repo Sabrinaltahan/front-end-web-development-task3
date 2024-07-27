bad8b093cb7d007ba978906223d37198142af52d
Sabrin Altahan <sabrinaltahan157@gmail.com>
2024-07-28 01:18:06 +0300

    updates in styles

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -2,5 +2,7 @@ document.getElementById("mode-toggle").addEventListener("click", function() {
     // Toggle theme class on body
     document.body.classList.toggle("dark");
 });
+// Automatically apply dark mode if user preference is dark
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) document.body.classList.add("dark");
 
 //# sourceMappingURL=index.de158e3a.js.map
---
+++ dist/index.ea0fb51e.js
@@ -0,0 +1,2 @@
+document.getElementById("mode-toggle").addEventListener("click",function(){document.body.classList.toggle("dark")}),window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches&&document.body.classList.add("dark");
+//# sourceMappingURL=index.ea0fb51e.js.map

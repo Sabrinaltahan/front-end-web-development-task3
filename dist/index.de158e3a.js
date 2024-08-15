3696ed825022bea2dcb8dfe668f6106d0c770903
Sabrin Altahan <sabrinaltahan157@gmail.com>
2024-08-15 22:15:28 +0300

    theme variable updates

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -1,8 +1,22 @@
 document.getElementById("mode-toggle").addEventListener("click", function() {
-    // Toggle theme class on body
-    document.body.classList.toggle("dark");
+    const body = document.body;
+    // Check the current mode
+    if (body.classList.contains("dark-mode")) {
+        console.log("the body has dart mode");
+        // Switch to light mode
+        body.classList.remove("dark-mode");
+        body.classList.add("light-mode");
+    } else {
+        console.log("the body has light mode");
+        // Switch to dark mode
+        body.classList.remove("light-mode");
+        body.classList.add("dark-mode");
+    }
 });
-// Automatically apply dark mode if user preference is dark
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) document.body.classList.add("dark");
+// Automatically apply the system preference on initial load
+window.addEventListener("load", ()=>{
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) document.body.classList.add("dark-mode");
+    else document.body.classList.add("light-mode");
+});
 
 //# sourceMappingURL=index.de158e3a.js.map
